03ce023994071729ce02ce2588f7ed34e9b0accf
Adityag0243 <mr.adityag123@gmail.com>
2025-08-26 00:16:48 +0530

    improving iterations

--- urbansole/src/components/shoe_list.jsx
+++ urbansole/src/components/shoe_list.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import shoes from "../data/shoes.json";
 
 
 import { Shoe_Card } from "./shoe_card";
  
 
-function ShoeList() {
-    
+function ShoeList({props}) {
+  console.log(Array.isArray(props));
+
   return (
     <>
-      <div className="w-full mx-auto px-20 py-12 mt-10 bg-white">
+      <div className="w-full px-20 py-12 mb-10 bg-white">
           <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-x-5 gap-y-7">
-            {shoes.map((shoe, index) => (
+            {props.map((shoe, index) => (
               <Shoe_Card key={index} {...shoe} />
             ))}
           </div>
