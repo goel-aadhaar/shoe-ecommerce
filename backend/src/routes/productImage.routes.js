9c89ec83134a33aa69ae7e0e7895cb3607dd4e40
goel-aadhaar <goel.aadhaar@outlook.com>
2025-09-12 06:09:52 +0530

    Fix import statement for upload middleware in productImage.routes.js

--- backend/src/routes/productImage.routes.js
+++ backend/src/routes/productImage.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import upload from "../middlewares/upload.middleware.js"; // multer middleware
+import {upload} from "../middlewares/upload.middleware.js"; // multer middleware
 import { addProductImageById } from "../controllers/productImage.controller.js";
 import { authMiddleware, adminMiddleware } from "../middlewares/auth.middleware.js";
 
