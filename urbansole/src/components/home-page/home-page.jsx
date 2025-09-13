e52b1f18e3d9eb3d2ecd91ee82accccab0085890
goel-aadhaar <goel.aadhaar@outlook.com>
2025-09-13 18:47:29 +0530

    Remove unused FilterBar import from Home component

--- urbansole/src/components/home-page/home-page.jsx
+++ urbansole/src/components/home-page/home-page.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'; // Make sure to import useEf
 import TrendingSection from "../trending-section/trending";
 import NewArrivalSection from "../New-Arrival Section/new-arrival-section"
 import BrandCarousel from "../brandCardCarousel";
-import FilterBar from "../filter";
 import AdCard from "./adCard1";
 import AdCard2 from "./adCard2";
 import AdCard3 from "./adCard3";
