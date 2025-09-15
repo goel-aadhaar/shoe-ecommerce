d5cec3c20902a2dbffdb55117572df4fb1507803
Adityag0243 <mr.adityag123@gmail.com>
2025-09-16 03:06:14 +0530

    populating ImageSet thumbnail

--- backend/src/controllers/cart.controller.js
+++ backend/src/controllers/cart.controller.js
@@ -9,7 +9,15 @@ export const getCart = asyncHandler(async (req, res) => {
     if (!cart) {
         cart = await Cart.create({ userId: req.user.id });
     }
-    const items = await CartItem.find({ cartId: cart._id }).populate("productId").populate('imageSet', 'thumbnail');
+   const items = await CartItem.find({ cartId: cart._id })
+        .populate({
+            path: "productId",          
+            populate: {
+                path: "imageSet",           
+                model: "ProductImage",
+                select: "thumbnail"
+            }
+        });
     res.status(200)
     .json(
         new ApiResponse(
