e724aa0433ea7b8fd3cf58ea085ec6f270431cfb
Adityag0243 <mr.adityag123@gmail.com>
2025-09-16 12:43:08 +0530

    fixing async handler

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -10,14 +10,16 @@
 // }
 
 export const asyncHandler = (requestHandler) => {
-    return async (req , res , next) => {
-        Promise.resolve(requestHandler(req , res , next))
-        .catch((error) => {
-            res.status(Number(error.code) || 500)
-            .json({
-                success: false,
-                message: error.message || "INTERNAL SERVER ERROR"
-            })
-        })
-    }
+    return async (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) => {
+        const statusCode = Number(error.code) || 500;
+        res.status(statusCode).json({
+        success: false,
+        message: error.message || "INTERNAL SERVER ERROR",
+        });
+    });
+    };
 }
+
+
+
