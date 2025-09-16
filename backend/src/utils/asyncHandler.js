2ab2dbee065520d0788ee8df3dff4b33ba72dfba
Adityag0243 <mr.adityag123@gmail.com>
2025-09-16 12:43:59 +0530

    fix

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -10,16 +10,14 @@
 // }
 
 export const asyncHandler = (requestHandler) => {
-    return async (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((error) => {
-        const statusCode = Number(error.code) || 500;
-        res.status(statusCode).json({
-        success: false,
-        message: error.message || "INTERNAL SERVER ERROR",
-        });
-    });
-    };
+    return async (req , res , next) => {
+        Promise.resolve(requestHandler(req , res , next))
+        .catch((error) => {
+            res.status(error.code || 500)
+            .json({
+                success: false,
+                message: error.message || "INTERNAL SERVER ERROR"
+            })
+        })
+    }
 }
-
-
-
