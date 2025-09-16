6898dbc28975512e6fe872c52571b0cffdbf2793
Adityag0243 <mr.adityag123@gmail.com>
2025-09-16 13:24:27 +0530

    Number(err.code)

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -13,7 +13,7 @@ export const asyncHandler = (requestHandler) => {
     return async (req , res , next) => {
         Promise.resolve(requestHandler(req , res , next))
         .catch((error) => {
-            res.status(error.code || 500)
+            res.status(Number(error.code) || 500)
             .json({
                 success: false,
                 message: error.message || "INTERNAL SERVER ERROR"
